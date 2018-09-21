1ee1e5f82728867149a43b5dfae31fe356618e03
cso <cso@cin.ufpe.br>
2018-09-20 23:38:42 -0300

    init of the problem

---
+++ Cpp/LeetCode/countAndSay.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string solution(int n){
+	string after = "1";	
+	for(int i = 1; i<n; ++i){
+			
+	}
+}
+
+int main(){
+	return 0;
+}
