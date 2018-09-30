5e3ae950405f07a79ed56e51dbf746bf8959bc56
cso <cso@cin.ufpe.br>
2018-09-30 13:34:22 -0300

    In progress

--- Cpp/LeetCode/BinaryTreeLevelOrderTraversalII.cpp
+++ Cpp/LeetCode/BinaryTreeLevelOrderTraversalII.cpp
@@ -12,15 +12,24 @@ struct TreeNode{
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-vector<vector<int>> levelOrderBottom(TreeNode* root){
+vector<vector<int> > levelOrderBottom(TreeNode* root){
 	if(root==NULL) return NULL;
+	TreeNode *cur;	
 	queue<TreeNode*> bfs_queue;
-	vector<vector<int>> result;
-	result.push_back();
+	vector<vector<int> > result;
+	result.push_back((vector<int>)(root->val));
 	bfs_queue.push(root);
 	while(!bfs_queue.empty()){
-
-	} 
+		cur = bfs_queue.pop();
+		if(cur->left != NULL && cur->right != NULL){}
+		else if(cur->left != NULL && cur->right == NULL){}
+		else if(cur->left == NULL && cur->right != NULL){}
+		result.push_back([cur->left->val, cur->right->val]);
+	}
+	for(vector<vector<int> >::iterator it = result.begin(); it!=result.end(); ++it){
+			
+	}
+	return result;
 }
 
 int main(){
---
+++ Python/leetCode/question_107_btlot.py~
@@ -0,0 +1,9 @@
+class TreeNode(object):
+	def _init_(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+Class Solution(object):
+	def levelOrderBottom(self,root):
+		
