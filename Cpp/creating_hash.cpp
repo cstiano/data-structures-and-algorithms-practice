340373c383a8c0edb5c76d040582cb46f4b22deb
cso <cso@cin.ufpe.br>
2018-10-03 23:59:11 -0300

    Including write and reading matrix

--- Cpp/creating_hash.cpp
+++ Cpp/creating_hash.cpp
@@ -3,8 +3,9 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 using namespace std;
-
+// g++ --std=c++11
 int main(){
 	vector<string> content;
 	map<string,int> states_mapping;
@@ -23,6 +24,33 @@ int main(){
 	// for(map<string,int>::iterator it = states_mapping.begin(); it!=states_mapping.end(); ++it){
 	// 	cout<<it->first<<" "<<it->second<<endl;
 	// }
-	cout<<states_mapping["1-1"]<<endl;
+	// cout<<states_mapping["1-1"]<<endl;
+	int m = 2, n = 2;
+	double A[2][2] = {{1.1,2.2},{3.3,4.4}};
+	double B[2][2];
+	
+	ofstream f("matrix.txt");
+	for (int i = 0; i < m; i++)
+	  {
+	  for (int j = 0; j < n; j++)
+	    {
+	    f << A[i][j]<<" ";
+	    }
+	  // f << "\n";
+	  }
+	  f.close();
+
+	ifstream f2("matrix.txt");
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < m; j++)
+		  f2 >> B[i][j];
+	}
+
+	for (int i = 0; i < m; i++){
+		for (int j = 0; j < m; j++)
+			cout<<B[i][j]<<" ";
+		cout<<endl;
+	}
+	f2.close();
 	return 0;
 }
